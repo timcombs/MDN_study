c7a89b523e7d47f4d6caebe5eafd698f552ac687
Tim Combs <tim00combs@gmail.com>
2019-07-02 17:19:23 -0700

    es6 study from Udemy, practice

---
+++ es6Stuff.js
@@ -0,0 +1,6 @@
+const es6Bucket = document.querySelector('#es6-study');
+
+var myName = 'tim';
+console.log(myName);
+
+es6Bucket.insertAdjacentHTML('beforeend', `<h3>es6 study below</h3><p>myName</p>`);
