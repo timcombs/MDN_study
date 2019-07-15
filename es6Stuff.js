750ea790d56dbb3f90296ea7dcc26ba7ad444dc9
Tim Combs <tim00combs@gmail.com>
2019-07-15 06:57:07 -0700

    cleaning up repository

--- es6Stuff.js
+++ es6Stuff.js
@@ -1,6 +1,5 @@
 const es6Bucket = document.querySelector('#es6-study');
 
 var myName = 'tim';
-console.log(myName);
 
 es6Bucket.insertAdjacentHTML('beforeend', `<h3>es6 study below</h3><p>myName</p>`);
